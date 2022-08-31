8c899744cb224fc295fd8fd593e1d88d66f12ddf
MMAzeem3 <musasa12399@gmail.com>
2022-08-31 16:17:04 -0400

    success

--- Lab1/sqrt.asm
+++ Lab1/sqrt.asm
@@ -5,13 +5,40 @@
 
 	li 	a7, 5
 	ecall			# get integer input value
-	
+		
 	slli	s0, a0, 14	# Convert input to (32,14) format
 	
+	
+	mul	s3, s2, s2
+	li	a7, 1
+	mv	a0, s3
+	ecall
+
 
 loop:
-	beq 	s0, zero, done
+	ble 	s1, zero, done
 	
+	mul	s3, s2, s2
+	mulhu	s4, s2, s2
+	
+	bne	s3, s0, continue
+	
+	j done
+	
+continue:
+	blt	s3, s0, lessthan
+	
+	sub	s2, s2, s1
+	srli	s1, s1, 1
+	j loop
+
+lessthan:
+	add	s2, s2, s1
+	srli	s1, s1, 1
+	j loop
 
 
 done:
+	li	a7, 1
+	mv	a0, s2
+	ecall
