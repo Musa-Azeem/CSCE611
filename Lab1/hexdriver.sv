0fd16442d3553ceb01ac6c9d8636e08e4754dd1c
Rahul Bulusu <71791195+rahulbulusu@users.noreply.github.com>
2022-09-21 16:59:04 -0400

    Update hexdriver.sv
    Added binary part which I believe should work. This is incase our top code doesn't work.

--- Lab1/hexdriver.sv
+++ Lab1/hexdriver.sv
@@ -13,5 +13,27 @@ module hexdriver (input [3:0] val, output logic [6:0] HEX);
 	assign HEX[5] = w | x | (~y & ~z);
 	assign HEX[6] = w | (~x & y) | (x & ~y) | (x & ~z);
 
-
+	//In case the top code doesn't work this is the binary way I think it should work need to test
+	/*
+	always_comb begin
+                case (in)
+                        4'h0: out = 7'b1000000;
+                        4'h1: out = 7'b1111001;
+                        4'h2: out = 7'b0100100;
+                        4'h3: out = 7'b0110000;
+                        4'h4: out = 7'b0011001;
+                        4'h5: out = 7'b0010010;
+                        4'h6: out = 7'b0000010;
+                        4'h7: out = 7'b1111000;
+                        4'h8: out = 7'b0000000;
+                        4'h9: out = 7'b0010000;
+                        4'hA: out = 7'b0001000;
+                        4'hB: out = 7'b0000011;
+                        4'hC: out = 7'b1000110;
+                        4'hD: out = 7'b0100001;
+                        4'hE: out = 7'b0000110;
+                        4'hF: out = 7'b0001110;
+                endcase
+        end
+	*/
 endmodule
