2828a69697982b2dc640c68e591435c6097fbc5d
Rahul Bulusu <71791195+rahulbulusu@users.noreply.github.com>
2022-09-21 17:07:01 -0400

    Update simtop.sv
    Finish the counter part TBD!

--- Lab1/simtop.sv
+++ Lab1/simtop.sv
@@ -37,16 +37,20 @@ module simtop;
 	);
 
 // your code here
+logic [31:0] c;  //initialize counter 
+initial begin
+	c = 0;
+end
 
 //the counter is connected to the HEX displays
-hexdecoder hex0(.in(counter[3:0]), .out(HEX0));
-hexdecoder hex1(.in(counter[7:4]), .out(HEX1));
-hexdecoder hex2(.in(counter[11:8]), .out(HEX2));
-hexdecoder hex3(.in(counter[15:12]), .out(HEX3));
-hexdecoder hex4(.in(counter[19:16]), .out(HEX4));
-hexdecoder hex5(.in(counter[23:20]), .out(HEX5));
-hexdecoder hex6(.in(counter[27:24]), .out(HEX6));
-hexdecoder hex7(.in(counter[31:28]), .out(HEX7));
+hexdecoder hex0(.in(c[3:0]), .out(HEX0));
+hexdecoder hex1(.in(c[7:4]), .out(HEX1));
+hexdecoder hex2(.in(c[11:8]), .out(HEX2));
+hexdecoder hex3(.in(c[15:12]), .out(HEX3));
+hexdecoder hex4(.in(c[19:16]), .out(HEX4));
+hexdecoder hex5(.in(c[23:20]), .out(HEX5));
+hexdecoder hex6(.in(c[27:24]), .out(HEX6));
+hexdecoder hex7(.in(c[31:28]), .out(HEX7));
 
 endmodule
 
