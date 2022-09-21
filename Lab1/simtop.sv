40d1050f1d9f3d0a78816dd59b0b640a7f8087a8
Rahul Bulusu <71791195+rahulbulusu@users.noreply.github.com>
2022-09-21 16:53:16 -0400

    Update simtop.sv
    Still need to write the code for the counter

--- Lab1/simtop.sv
+++ Lab1/simtop.sv
@@ -38,5 +38,15 @@ module simtop;
 
 // your code here
 
+//the counter is connected to the HEX displays
+hexdecoder hex0(.in(counter[3:0]), .out(HEX0));
+hexdecoder hex1(.in(counter[7:4]), .out(HEX1));
+hexdecoder hex2(.in(counter[11:8]), .out(HEX2));
+hexdecoder hex3(.in(counter[15:12]), .out(HEX3));
+hexdecoder hex4(.in(counter[19:16]), .out(HEX4));
+hexdecoder hex5(.in(counter[23:20]), .out(HEX5));
+hexdecoder hex6(.in(counter[27:24]), .out(HEX6));
+hexdecoder hex7(.in(counter[31:28]), .out(HEX7));
+
 endmodule
 
