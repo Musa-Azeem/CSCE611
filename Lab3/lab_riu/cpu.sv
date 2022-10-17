ff154ebd125d2e806a3f1edb18f1c42a9703ced1
MMAzeem3 <musa.mazeem@gmail.com>
2022-10-17 17:12:16 -0400

    progress

--- Lab3/lab_riu/cpu.sv
+++ Lab3/lab_riu/cpu.sv
@@ -1,6 +1,6 @@
 module cpu (
-    input logic clk, rst_n,
-    output logic[7:0] HEX[4:0]
+    input logic         clk, rst_n,
+    output logic[7:0]   HEX[4:0]
     )
 
     logic [31:0] inst_ram [4191:0];
@@ -9,7 +9,7 @@ module cpu (
     logic [11:0] PC_FETH = 12'd0;
     logic [31:0] instruction_EX;
 
-    always @(posedge clk) begin
+    always_ff @(posedge clk) begin
         if (~rst_n) begin
             PC_FETCH <= 12'd0;
             instruction_EX <= 32'd0;
@@ -20,12 +20,35 @@ module cpu (
         end
     end
 
+    // decode instruction
+    assign logic [6:0] funct7_EX = instruction_EX[31:25];   // R-type
+    logic [4:0]  rs1_EX = instruction_EX[19:15];            // R-type  I-type
+    logic [4:0]  rs2_EX = instruction_EX[24:20];            // R-type
+    logic [4:0]  rd_EX = instruction_EX[11:7];              // R-type  I-type
+    logic [2:0]  funct3_EX = instruction_EX[14:12];         // R-type  I-type
+    logic [6:0]  opcode_EX = instruction_EX[6:0];           // R-type  I-type
+    logic [11:0] imm12_EX = instruction_EX[31:20];          //         I-type
+    logic [19:0] imm20_EX = instruction_EX[31:12];          // U-type
+    // TODO J-type
+
     // check opcode to get instruction decoding type
+    logic [1:0] instruction_type;
     always_comb begin
-        case(instruction_EX[6:0])
-            7'b0110011: // TODO R-type
-            7'b0010011: // TODO I-type
-            7'b0110111: // TODO U-type (lui)
+        case(opcode_EX)
+            7'b0110011: instruction_type = 2'b00;    // R-type
+            7'b0010011: instruction_type = 2'b01;    // I-type
+            7'b0110111: instruction_type = 2'b10;    // U-type (lui)
+            // TODO J-type is 3
         endcase
     end
+
+    logic [3:0] aluop;
+    logic alusrc;
+    logic regsel;
+    logic regwrite;
+    logic gpio_we;
+
+    // TODO set control signals with modules for R, I, and U type
+
+
 endmodule
