1d1894926489e1dda56051f057a5ddfc068a616e
MMAzeem3 <musa.mazeem@gmail.com>
2022-10-22 01:23:04 -0400

    .

--- Lab3/lab_riu/control_fields.sv
+++ Lab3/lab_riu/control_fields.sv
@@ -13,30 +13,30 @@ module control_fields(
     // First, IO type (csrrw) control fields
 	if (opcode == 7'h73 && funct3 == 3'b001) begin
         // Don't set aluop or alusrc - ALU not used for IO
-        if (imm12 == 12'hf02) {
+        if (imm12 == 12'hf02) begin
             // If the immediate is the second IO port, we are writing to HEX
             // Don't set regsel - not writing to registers
             regwrite = 0;           // disable write to register
             gpio_we = 1;            // enable to write to IO port
-        }
-        else if (imm12 == 7'hf00) {
+        end
+        else if (imm12 == 7'hf00) begin
             // If the immediate is the first IO port, we are reading from switches
             regsel = 2'b00;         // Set to 0 to read IO port to EX/WB pipeline register
             regwrite = 1;           // Enable write to register
             gpio_we = 0;            // Disable write to IO port
-        }
+        end
     end
 
     // Next, U-type (lui) control fields
-    else if (opcode == 7'h37) {
+    else if (opcode == 7'h37) begin
         // Don't set aluop or alusrc - ALU not used for U-type
         regsel = 2'b01;             // Set to 1 to read imm20 to EX/WB pipeline register
         regwrite = 1;               // Enable write to register
         gpio_we = 0;                // Disable write to IO
-    }
+    end
 
     // Next, R-type control fields
-    else if (opcode == 7'h33) {
+    else if (opcode == 7'h33) begin
         alusrc = 0;                 // Second input to ALU is value from register
         regsel = 2'b10;             // Set to 2 to read ALU output to EX/WB pipeline register
         regwrite = 1;               // Enable write to register
@@ -73,10 +73,10 @@ module control_fields(
             end
 
         endcase
-    } 
+    end
 
     // Next I-type control fields (excluding aluop)
-    else if (opcode == 7'h13) {
+    else if (opcode == 7'h13) begin
         alusrc = 1;                 // Second input to ALU is sign extended imm12
         regsel = 2'b10;             // Set to 2 to read ALU output to EX/WB pipeline register
         regwrite = 1;               // Enable write to register
@@ -98,6 +98,6 @@ module control_fields(
                 endcase
             end
         endcase
-    }
+    end
 
 endmodule
