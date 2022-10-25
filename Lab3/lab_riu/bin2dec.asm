31ac79962cc2533295a7da7929bff378a3cbb46a
Rahul Bulusu <71791195+rahulbulusu@users.noreply.github.com>
2022-10-24 22:10:42 -0400

    will fix tmrw

--- Lab3/lab_riu/bin2dec.asm
+++ Lab3/lab_riu/bin2dec.asm
@@ -1,16 +1,16 @@
-li	x10,10
-li  x11,429496730
+li	a0,10
+li  a1,429496730
 
-add	x15	x0	x0	# clear registers 15, 5, 6
-add	x5	x0	x0
-add	x6	x0	x0
+add	a2,zero,zero
+add	t0,zero,zero
+add	t1,zero,zero
 
-csrrw	x5	io0	x0
+csrrw	t0	hex_value, zero
 
-mul	x6	x5	x11	# x6 is the fractional part
-mulhu	x5	x5	x11	# x5 is the running value
-mulhu	x6	x6	x10	# multiply by 10, x6 is now the modulus
-slli	x6	x6	0
+mul	t1,t0,a1	#t1 is the fractional part
+mulhu	t0,t0,a1
+mulhu	t1,t1,a0	# multiply by 10 and t1 becomes the modulus
+slli	t1,t1,0
 or	x15	x15	x6
 
 mul	x6	x5	x11	#  x6 is the fractional part
