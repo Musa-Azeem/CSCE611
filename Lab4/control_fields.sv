90cfd3fb6b82e4f8e2865ee3e7c3bb4e79571793
MMAzeem3 <musa.mazeem@gmail.com>
2022-11-02 16:33:23 -0400

    starting new lab

---
+++ Lab4/control_fields.sv
@@ -0,0 +1,131 @@
+module control_fields(
+    input               [6:0]       funct7,
+    input               [2:0]       funct3,
+    input               [6:0]       opcode,
+    input               [11:0]      imm12,
+    output logic        [3:0]       aluop,
+    output logic        [1:0]       regsel,
+    output logic                    alusrc, regwrite, gpio_we
+    );
+
+    // Find control signals
+
+    always_comb begin
+        // First, IO type (csrrw) control fields
+        if (opcode == 7'h73) begin
+            // Don't care about aluop or alusrc - ALU not used for IO
+            aluop = 4'bx;
+            alusrc = 1'bx;
+            if (imm12 == 12'hf02) begin
+                // If the immediate is the second IO port, we are writing to HEX
+                // Don't care about regsel - not writing to registers
+                regsel = 2'bx;
+                regwrite = 0;           // disable write to register
+                gpio_we = 1;            // enable to write to IO port
+            end
+            else if (imm12 == 12'hf00) begin
+                // If the immediate is the first IO port, we are reading from switches
+                regsel = 2'b00;         // Set to 0 to read IO port to register
+                regwrite = 1;           // Enable write to register
+                gpio_we = 0;            // Disable write to IO port
+            end
+            else begin
+                // Never reached
+                regsel = 2'bx;
+                regwrite = 1'bx;
+                gpio_we = 1'bx;
+            end
+        end
+
+        // Next, U-type (lui) control fields
+        else if (opcode == 7'h37) begin
+            // Don't care about aluop or alusrc - ALU not used for U-type
+            aluop = 4'bx;
+            alusrc = 1'bx;
+            regsel = 2'b01;             // Set to 1 to read imm20 to register
+            regwrite = 1;               // Enable write to register
+            gpio_we = 0;                // Disable write to IO
+        end
+
+        // Next, R-type control fields
+        else if (opcode == 7'h33) begin
+            alusrc = 0;                 // Second input to ALU is value from register
+            regsel = 2'b10;             // Set to 2 to read ALU output to EX/WB pipeline register
+            regwrite = 1;               // Enable write to register
+            gpio_we = 0;                // Disable write to IO
+
+            // Get aluop based on funct7 and funct3
+            case(funct7)
+                7'h0: begin
+                    case (funct3)
+                        3'b000:     aluop = 4'b0011;        // add
+                        3'b001:     aluop = 4'b1000;        // sll      (<<)
+                        3'b010:     aluop = 4'b1100;        // slt      (<)
+                        3'b011:     aluop = 4'b1101;        // sltu     (< unsigned)
+                        3'b100:     aluop = 4'b0010;        // xor
+                        3'b101:     aluop = 4'b1001;        // srl      (>>)
+                        3'b110:     aluop = 4'b0001;        // or
+                        3'b111:     aluop = 4'b0000;        // and
+                        default:    aluop = 4'bx;           // Never reached
+                    endcase
+                end
+
+                7'h1: begin
+                    case (funct3)
+                        3'b000:     aluop = 4'b0101;       // mul
+                        3'b001:     aluop = 4'b0110;       // mulh
+                        3'b011:     aluop = 4'b0111;       // mulhu     
+                        default:    aluop = 4'bx;           // Never reached
+                    endcase
+                end
+
+                7'h20: begin
+                    case (funct3)
+                        3'b000:     aluop = 4'b0100;       // sub
+                        3'b101:     aluop = 4'b1010;       // sra      (>>>)
+                        default:    aluop = 4'bx;           // Never reached
+                    endcase
+                end
+
+                default: aluop = 4'bx;      // Never reached
+            endcase
+        end
+
+        // Next I-type control fields (excluding aluop)
+        else if (opcode == 7'h13) begin
+            alusrc = 1;                 // Second input to ALU is sign extended imm12
+            regsel = 2'b10;             // Set to 2 to read ALU output to EX/WB pipeline register
+            regwrite = 1;               // Enable write to register
+            gpio_we = 0;                // Disable write to IO   
+
+            // Get aluop based on funct3
+            case (funct3)
+                3'b000:     aluop = 4'b0011;        // addi
+                3'b001:     aluop = 4'b1000;        // slli 
+                3'b100:     aluop = 4'b0010;        // xori
+                3'b110:     aluop = 4'b0001;        // ori
+                3'b111:     aluop = 4'b0000;        // andi
+
+                // Shift instructions - choose aluop based on first 7 bits of imm12
+                3'b101: begin
+                    case (imm12[11:5])
+                        7'h0:   aluop = 4'b1001;    // srli
+                        7'h20:  aluop = 4'b1010;    // srai
+                        default: aluop = 4'bx;      // Never reached
+                    endcase
+                end
+                default: aluop = 4'bx;              // Never reached
+            endcase
+        end
+
+        else begin
+            // Never reached with R-type, I-type, U-type, or IO-type
+            aluop = 4'bx;
+            alusrc = 1'bx;
+            regsel = 2'bx;
+            regwrite = 1'bx;
+            gpio_we = 1'bx;
+        end
+    end
+
+endmodule
---
+++ Lab4/simtop.sv
@@ -0,0 +1,97 @@
+/* Copyright 2020 Jason Bakos, Philip Conrad, Charles Daniels */
+
+/* Top-level module for CSCE611 RISC-V CPU, for running under simulation.  In
+ * this case, the I/Os and clock are driven by the simulator. */
+
+module simtop;
+
+	logic clk;
+	logic 	[6:0] 		HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7;
+	logic 	[17:0]		SW;
+	logic	[3:0]		KEY;
+	logic	[8:0]		LEDG;
+	logic	[17:0]		LEDR;
+
+
+	top dut
+	(
+		//////////// CLOCK //////////
+		.CLOCK_50(clk),
+		.CLOCK2_50(clk),
+	    .CLOCK3_50(clk),
+
+		//////////// LED //////////
+		.LEDG(LEDG),
+		.LEDR(LEDR),
+
+		//////////// KEY //////////
+		.KEY(KEY),
+
+		//////////// SW //////////
+		.SW(SW),
+
+		//////////// SEG7 //////////
+		.HEX0(HEX0),
+		.HEX1(HEX1),
+		.HEX2(HEX2),
+		.HEX3(HEX3),
+		.HEX4(HEX4),
+		.HEX5(HEX5),
+		.HEX6(HEX6),
+		.HEX7(HEX7)
+	);
+
+	// SIMULATE CLOCK
+    always begin
+		#5 clk = 1;
+		#5 clk = 0;
+	end
+
+	// Read expected values
+	logic [31:0] expected_values [4191:0];
+    initial $readmemh("testbench-expected.rom", expected_values);
+
+	// SIMULATE INPUT
+	integer i;
+	initial begin	
+		SW = {2'h0, 4'h0, 4'h0, 4'h0, 4'h1};
+		KEY = 4'b0;		// reset
+
+		#10;
+		KEY = 4'b1;
+
+		#10;
+		#10;
+		#10;
+		if (dut.mcpu.mregfile.mem[1] != expected_values[0]) $display("Test case 0 incorrect");
+		#10;
+		if (dut.mcpu.mregfile.mem[2] != expected_values[1]) $display("Test case 1 incorrect");
+		#10;
+		if (dut.mcpu.mregfile.mem[3] != expected_values[2]) $display("Test case 2 incorrect");
+		#10;
+		if (dut.mcpu.mregfile.mem[3] != expected_values[3]) $display("Test case 3 incorrect");
+		#10;
+		if (dut.mcpu.mregfile.mem[3] != expected_values[4]) $display("Test case 4 incorrect");
+		#10;
+		if (dut.mcpu.mregfile.mem[4] != expected_values[5]) $display("Test case 5 incorrect");
+		#10;
+		if (dut.mcpu.mregfile.mem[4] != expected_values[6]) $display("Test case 6 incorrect");
+		#10;
+		if (dut.mcpu.mregfile.mem[5] != expected_values[7]) $display("Test case 7 incorrect");
+		#10;
+		if (dut.mcpu.mregfile.mem[6] != expected_values[8]) $display("Test case 8 incorrect");
+		#10;
+		if (dut.mcpu.mregfile.mem[6] != expected_values[9]) $display("Test case 9 incorrect");
+		#10;
+
+		for (i=7; i<20; i=i+1) begin
+			if (dut.mcpu.mregfile.mem[i] != expected_values[i+3]) $display("Test case %i incorrect", (i+3));
+			#10;
+		end
+
+		if (dut.display != expected_values[24]) $display("Test case 24 incorrect");
+		if (dut.mcpu.mregfile.mem[21] != expected_values[25]) $display("Test case 25 incorrect");
+	end
+
+endmodule
+
